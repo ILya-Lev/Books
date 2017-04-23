30e043b81eedc2fea6274e85d496986e6b600bfd
ILya-Lev <lis2889@gmail.com>
2017-04-24 00:46:25 +0300

    Add project files.

---
+++ Books.Entities/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Books.Entities
+{
+	public enum Genre
+	{
+		[Display(Name = "Non Fiction")]
+		NonFiction,
+		Action,
+		Romance,
+		[Display(Name = "Science Fiction")]
+		ScienceFiction
+	}
+}
---
+++ Books.Web/Helpers/EnumHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Books.Web.Helpers
+{
+	public static class EnumHelpers
+	{
+		public static string GetDisplayName(this Type enumType, int? selectedValue)
+		{
+			var name = Enum.GetName(enumType, selectedValue);
+			var displayName = enumType.GetField(name)
+				.GetCustomAttributes(inherit: false)
+				.OfType<DisplayAttribute>()
+				.FirstOrDefault()?.Name;
+
+			return displayName ?? name;
+		}
+	}
+}
